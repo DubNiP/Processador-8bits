b15b69814a303da7507afed993555b1f02696eb8
Dub <rodrigodubm@gmail.com>
2026-06-28 16:50:13 -0300

    Arquivo top-level quase pronto e modificações pertinentes

--- PC.vhd
+++ PC.vhd
@@ -2,8 +2,8 @@ library IEEE; use IEEE.STD_LOGIC_1164.all;
 entity PC is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           pc_in : in  STD_LOGIC_VECTOR (31 downto 0);
-           pc_out : out  STD_LOGIC_VECTOR (31 downto 0));
+           pc_in : in  STD_LOGIC_VECTOR (2 downto 0);
+           pc_out : out  STD_LOGIC_VECTOR (2 downto 0));
 end PC;
 
 architecture Behavioral of PC is
